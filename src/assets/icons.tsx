f5755d3c7843aff7671f058385a7502cc5fad9f0
simonbandmann <s.bandmann@gmail.com>
2024-01-27 23:33:29 +0100

    feat: adjust nav styles

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -9,7 +9,7 @@ export const Bars = () => (
         <path
             strokeLinecap='round'
             strokeLinejoin='round'
-            d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+            d='M3.75 9h16.5m-16.5 6.75h16.5'
         />
     </svg>
 )
@@ -25,7 +25,7 @@ export const Close = () => (
         <path
             strokeLinecap='round'
             strokeLinejoin='round'
-            d='m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
+            d='M6 18 18 6M6 6l12 12'
         />
     </svg>
 )
